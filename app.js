743ce3439ee24485bd158f6398f2f1c79bb62dc9
NguyenNgocLam <lamnn8@fpt.com.vn>
2019-09-08 09:13:05 +0700

    create layout1

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express =  require('express')
 const app = express()
+const readLine = require('readline') ;
+const rl = readLine.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
 app.use(express.static("public")) ;
 app.set("view engine","ejs") ;
 app.set("views",'./views')
 app.get('/',(rep,res)=>{
-    res.render("layout1");
+    
+         res.render('layout1')
+     
 });
 module.exports = app
